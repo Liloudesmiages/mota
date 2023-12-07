a97f99a190b978ed200a2ab4e015d610f8408a83
Aurélie Parret <audatika@hotmail.fr>
2023-12-07 11:58:43 +0100

    Thème-menu

--- functions.php
+++ functions.php
@@ -2,15 +2,23 @@
 // Ajouter des styles
 function theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_uri());
-    wp_enqueue_style('theme-style-css', get_stylesheet_directory_uri().'/assets/css/style.css');
+    wp_enqueue_style('theme-style-css', get_stylesheet_directory_uri().'/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
 
-// declaration de menus dans wordpress
-function monmenu() {
-    register_nav_menu('header',__( 'Header' ));
-    register_nav_menu('footer',__( 'Footer' ));
-}
+function custom_theme_setup() {
+    // Enregistrement de l'emplacement du menu
+    register_nav_menus( array(
+        'menu-principal' => esc_html__( 'Menu Principal', 'mota' ),
+    ) );
 
-add_action( 'init', 'monmenu' );
+    // Gestion du logo
+    add_theme_support( 'custom-logo', array(
+        'height'      => 100, 
+        'width'       => 400, 
+        'flex-height' => true,
+        'flex-width'  => true,
+    ) );
+}
+add_action( 'after_setup_theme', 'custom_theme_setup' );
--- header.php
+++ header.php
@@ -1,23 +1,36 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?></title>
-  <?php wp_head()  // Code nécessaire dans le header au fonctionnement de wordpress ?>
+  <?php wp_head(); // Code nécessaire dans le header au fonctionnement de WordPress ?>
 </head>
-<header>
-  <div class="logo">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.svg" alt="Logo">
-  </div>
-  <nav>
-    <?php wp_nav_menu( array('theme_location' => 'header', 'container_class' => 'menu-header') ); ?>
-  </nav>
-</header>
-<section class="imageaccueil">
-  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/nathalie-11.jpeg" alt="">
-</section>
+<!--  pratique standard pour les thèmes WordPress, permet d'ajouter des classes spécifiques à la page -->
+<body <?php body_class(); ?>>
+
+<header id="masthead" class="site-header">
+    <div class="logo">
+        <?php if ( has_custom_logo() ) : ?>
+            <div class="site-logo">
+                <?php the_custom_logo(); ?>
+            </div>
+        <?php endif; ?>
+    </div>
+
+    <nav class="main-navigation">
+        <?php
+            wp_nav_menu( array(
+                'theme_location' => 'menu-principal', 
+                'menu_id'        => 'primary-menu',
+                'container'      => 'div', 
+                'container_class'=> 'menu-{menu slug}-container',
+                'menu_class'     => 'menu', 
+                ) );
+                ?>
+            </nav>
+        </header> 
+
+
 
 
 
